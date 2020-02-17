3cfabc9b5cbc162a26e79b0a60ddda1540117beb
larkeith <llarkeith@gmail.com>
2020-02-17 02:34:18 -0800

    Update application

--- application.py
+++ application.py
@@ -8,29 +8,33 @@ application = Flask(__name__)
 
 @application.route('/search')
 def search():
-    """ when someone types /search in the url this function will work to
-    present what we want for this page """
-    with psycopg2.connect(dbname=config("DB_DB"),
-    user=config("DB_USER"), password=config("DB_PASSWORD"),
-    host=config("DB_HOST"), port=config("DB_PORT")) as psql_conn:
-        output = get_jobs(psql_conn)
-    return jsonify(output)
+	""" when someone types /search in the url this function will work to
+	present what we want for this page """
+	with psycopg2.connect(dbname=config("DB_DB"),
+	user=config("DB_USER"), password=config("DB_PASSWORD"),
+	host=config("DB_HOST"), port=config("DB_PORT")) as psql_conn:
+		output = get_jobs(psql_conn)
+	ret = {
+		'count': len(output),
+		'responses': output
+	}
+	return jsonify(ret)
 
 
 
 @application.route('/details')
 def details():
-    """ when someone types /details in the url this function will work to
-    present what we want for this page """
-    input = request.get_json()
-    job_id = input.get('job_id', None)
-    if job_id is None:
-        raise NotImplementedError('not')
-    with psycopg2.connect(dbname=config("DB_DB"),
-    user=config("DB_USER"), password=config("DB_PASSWORD"),
-    host=config("DB_HOST"), port=config("DB_PORT")) as psql_conn:
-        output = get_details(job_id, psql_conn)
-    return jsonify(output)
+	""" when someone types /details in the url this function will work to
+	present what we want for this page """
+	args = request.get_json()
+	job_id = args.get('job_id', None)
+	if job_id is None:
+		raise NotImplementedError('not')
+	with psycopg2.connect(dbname=config("DB_DB"),
+	user=config("DB_USER"), password=config("DB_PASSWORD"),
+	host=config("DB_HOST"), port=config("DB_PORT")) as psql_conn:
+		output = get_details(job_id, psql_conn)
+	return jsonify(output)
 
 if __name__ == "__main__":
-    application.run()
+	application.run()
